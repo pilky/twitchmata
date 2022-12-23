411ca66af4f55e64ebd4283b13c1876dedcb680b
Martin Pilkington <pilky@mcubedsw.com>
2022-12-23 17:58:07 +0000

    Implement chat commands (fixes #15)

--- FeatureManagers/ChatCommandManager.cs
+++ FeatureManagers/ChatCommandManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Net;
 using TwitchLib.Client.Events;
 using TwitchLib.Client.Models;
+using TwitchLib.Communication.Events;
 using TwitchLib.Unity;
 using UnityEngine;
 
@@ -16,11 +17,55 @@ namespace Twitchmata {
         }
 
         private void Client_OnChatCommandReceived(object sender, OnChatCommandReceivedArgs args) {
-            this.ReceivedCommand(args.Command);
+            var command = args.Command;
+            if (this.RegisteredCommands.ContainsKey(command.CommandText) == false) {
+                return;
+            }
+
+            var user = this.UserManager.UserForChatMessage(command.ChatMessage);
+
+            var registeredCommand = this.RegisteredCommands[command.CommandText];
+            if (this.IsUserPermitted(user, registeredCommand.Permissions) == false) {
+                this.Manager.Client.SendRaw(":tmi.twitch.tv NOTICE #pilkycrc :Here is a notice");
+                this.Manager.Client.SendMessage(this.Manager.ConnectionConfig.ChannelName, "You don't have permission to use this command");
+                return;
+            }
+
+            registeredCommand.Callback.Invoke(command.ArgumentsAsList, user);
         }
 
-        public virtual void ReceivedCommand(TwitchLib.Client.Models.ChatCommand command) {
-            Debug.Log("Chat command received: " + command.CommandText);
+
+        #region Permissions
+        private bool IsUserPermitted(Models.User user, Permissions permissions) {
+            if ((permissions & Permissions.Chatters) == Permissions.Chatters) {
+                return true;
+            }
+            if (user.IsBroadcaster) {
+                return false;
+            }
+            if ((permissions & Permissions.Mods) == Permissions.Mods && user.IsModerator) {
+                return true;
+            }
+            if ((permissions & Permissions.VIPs) == Permissions.VIPs && user.IsVIP) {
+                return true;
+            }
+            if ((permissions & Permissions.Subscribers) == Permissions.Subscribers && user.IsSubscriber) {
+                return true;
+            }
+            return false;
         }
+        #endregion
+
+
+
+        #region Command Registration
+        private Dictionary<string, RegisteredChatCommand> RegisteredCommands = new Dictionary<string, RegisteredChatCommand>();
+        public void RegisterChatCommand(string command, Permissions permissions, ChatCommandCallback callback) {
+            this.RegisteredCommands[command] = new RegisteredChatCommand() {
+                Permissions = permissions,
+                Callback = callback,
+            };
+        }
+        #endregion
     }
 }
---
+++ Models/ChatCommands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Twitchmata {
+    public delegate void ChatCommandCallback(List<string> arguments, Models.User user);
+
+    internal struct RegisteredChatCommand {
+        internal Permissions Permissions;
+        internal ChatCommandCallback Callback;
+    }
+
+    [Flags]
+    public enum Permissions {
+        None = 0,
+        Mods = 1,
+        VIPs = 2,
+        Subscribers = 4,
+        Chatters = 8
+    }
+}
